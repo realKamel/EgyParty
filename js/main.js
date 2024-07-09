c26d219ed0eff615c7a5d339d97e04773717ee6f
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-07-10 01:49:30 +0300

    fin 2 sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 /// <reference types="../@types/jquery" />
 "use strict"
 
-$( '.aside .menu-icon span' ).on( "click", function () {
-	$( '.aside .menu-content ' ).animate( { width: 'toggle' }, 500 );
+$( '#openBtn' ).on( "click", function () {
+	$( '#leftMenu' ).animate( { width: '250px' }, 50 );
+	$( '#home-content' ).animate( { marginLeft: "250px" }, 50 );
+	$( '#openBtn' ).animate( { marginLeft: "250px" }, 50 );
+
+} )
+$( '.sidenav .closebtn' ).on( "click", function () {
+	$( '#leftMenu' ).animate( { width: '0' }, 50 );
+	$( '#home-content' ).animate( { marginLeft: "0" }, 50 );
+	$( '#openBtn' ).animate( { marginLeft: "0" }, 50 );
+} )
+
+$( "#leftMenu a" ).on( "click", function () {
+	let sectionId = $( this ).attr( "href" );
+	let positionOfSection = $( sectionId ).offset().top;
+	$( "html , body" ).animate( { scrollTop: positionOfSection }, 2000 );
+
+} )
+
+
+$( ".singer" ).on( "click", function ( e ) {
+	let currentState = $( e.currentTarget.nextElementSibling ).css( "display" );
+	if ( currentState !== "block" ) {
+		$( '.song' ).slideUp( 500 );
+		$( e.currentTarget.nextElementSibling ).slideDown( 500 );
+	} else {
+		console.log( "already displayed" );
+	}
 } )
