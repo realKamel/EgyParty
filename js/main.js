64e509ef93a0d0483254513db025beb16f217daf
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-07-09 17:32:32 +0300

    homePage init code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 /// <reference types="../@types/jquery" />
+"use strict"
+
+$( '.aside .menu-icon span' ).on( "click", function () {
+	$( '.aside .menu-content ' ).animate( { width: 'toggle' }, 500 );
+} )
